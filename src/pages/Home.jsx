bf685c2eafa69051c99d0f3a4097cddabada028b
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2025-07-01 21:52:34 -0300

    inicializacao da aba home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,19 @@
+import { FaSearch } from "react-icons/fa";
+import Button from "../components/button/Button";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className="banner-home">
+      <h1>Troque, Reutilize, Sustente</h1>
+      <p>Uma plataforma para trocar itens de forma sustentável e consciente.</p>
+      <p> Dê nova vida aos seus objetos e encontre o que precisa.</p>
+      <div className="container-search">
+        <div className="banner-input">
+          <FaSearch className="search-icon" />
+          <input type="text" placeholder="O que está procurando?" />
+        </div>
+        <Button link={"http://localhost:5173/"} text={"Buscar"} />
+      </div>
+    </section>
+  );
 }
