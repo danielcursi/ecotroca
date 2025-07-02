8ec1ebec43cef950ff30d677bef4ea00fc732b9a
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2025-07-02 20:03:02 -0300

    secao de informacoes concluida

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,37 @@
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaRecycle, FaUsers, FaBoxOpen } from "react-icons/fa";
 import Button from "../components/button/Button";
 
 export default function Home() {
   return (
-    <section className="banner-home">
-      <h1>Troque, Reutilize, Sustente</h1>
-      <p>Uma plataforma para trocar itens de forma sustentável e consciente.</p>
-      <p> Dê nova vida aos seus objetos e encontre o que precisa.</p>
-      <div className="container-search">
-        <div className="banner-input">
-          <FaSearch className="search-icon" />
-          <input type="text" placeholder="O que está procurando?" />
+    <>
+      <section className="banner-home">
+        <h1>Troque, Reutilize, Sustente</h1>
+        <p>
+          Uma plataforma para trocar itens de forma sustentável e consciente.
+        </p>
+        <p> Dê nova vida aos seus objetos e encontre o que precisa.</p>
+        <div className="container-search">
+          <div className="banner-input">
+            <FaSearch className="search-icon" />
+            <input type="text" placeholder="O que está procurando?" />
+          </div>
+          <Button link={"http://localhost:5173/"} text={"Buscar"} />
         </div>
-        <Button link={"http://localhost:5173/"} text={"Buscar"} />
-      </div>
-    </section>
+      </section>
+      <section className="section-infos">
+        <article>
+          <FaRecycle className="i" />
+          <p>Troca de Itens</p>
+        </article>
+        <article>
+          <FaUsers className="i" />
+          <p>Usuários Ativos</p>
+        </article>
+        <article>
+          <FaBoxOpen className="i" />
+          <p>Diversos Produtos</p>
+        </article>
+      </section>
+    </>
   );
 }
