d4ab2350aa20b81a98bcec10b0f6f69464541f4c
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2025-07-03 21:46:14 -0300

    pagina home concluida

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { FaSearch, FaRecycle, FaUsers, FaBoxOpen } from "react-icons/fa";
+import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import Button from "../components/button/Button";
+import data from "../components/database.json";
 import Card from "../components/card/Card";
 
 export default function Home() {
@@ -36,8 +38,31 @@ export default function Home() {
       <section className="section-emphasis">
         <h1>Itens em Destaque</h1>
         <p>Descubra itens únicos disponíveis para troca em sua região</p>
-        <section className="section-cards"></section>
+        <section className="section-cards">
+          {data.map((item, index) =>
+            index < 4 ? (
+              <Card
+                key={item.id}
+                img={item.image}
+                title={item.title}
+                description={item.description}
+                location={item.location}
+              />
+            ) : null
+          )}
+        </section>
       </section>
+      <footer className="footer">
+        <h2>Pronto para fazer parte da mudança?</h2>
+        <p>
+          Junte-se à nossa comunidade e contribua para um mundo mais sustentável
+        </p>
+        <Button
+          to={"/item"}
+          text={"Explorar itens"}
+          icon={faArrowRightToBracket}
+        />
+      </footer>
     </>
   );
 }
